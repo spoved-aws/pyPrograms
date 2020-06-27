6d2e8987f510f67e973b014c57fabaca9d0cff83
kanukhosla <kanukhosla10@gmail.com>
2020-06-26 19:04:21 -0700

    adding app.py file

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
+import os
 from flask import Flask
+app = Flask(__name__)
 
-application = Flask(__name__)
-@application.route("/")
- def hello():
- return "Hello World!"
+@app.route("/")
+def main():
+    return "Welcome!"
 
+@app.route('/how are you')
+def hello():
+    return 'I am good, how about you?'
 
 if __name__ == "__main__":
- application.run()
+    app.run(host="0.0.0.0", port=8080)
